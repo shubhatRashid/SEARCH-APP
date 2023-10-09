95c8ce3129a4fbc5efe3d86626b49e9594a930b5
shubhat33 <rashidshubhat41@gmail.com>
2023-10-09 19:26:26 +0530

    Initiate Create App,Setup app,add tailwind and create search bar component

---
+++ src/components/mainComponents/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-end pr-[5%] h-[15%] w-screen '>
+        <img className='w-[100px] mix-blend-color-burn' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuDWYEBi219Q_QseU5r63bhbrMot0Cm5Lf9O51WN2nJQ&s'/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/mainComponents/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header"
+import SearchBar from "./SearchBar"
+import SearchResult from "./SearchResult"
+import Suggestion from "./Suggestion"
+import Footer from "./Footer"
+
+export {Header,SearchBar,SearchResult,Suggestion,Footer}
